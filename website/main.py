bc6df46023463733455f87e8258383b377d3ceea
Breichman <br@rgroup2003.com>
2017-12-03 00:57:31 -0400

    Website so far

--- website/main.py
+++ website/main.py
@@ -11,7 +11,38 @@ def landingpage_with_survey():
 @app.route(r'/processing', methods=["POST"])
 def gettingdata():
     request.form["gender"]
-    print(request.form.get)
+    gender = request.form.get["gender"]
+    city = request.form.get["city"]
+    height = request.form.get["height"]
+    smoke = request.form.get["smoke"]
+    age = request.form.get["age"]
+    states = request.form.get["states"]
+    weight = request.form.get["weight"]
+    income = request.form.get["income"]
+    coverage = request.form.get["gender"]
+    married_status = request.form.get["gender"]
+    op1 = request.form.get["gender"]
+    opother = request.form.get["gender"]
+    try:
+        c1 = request.form.get["c1"]
+    except:
+        pass
+    try:
+        c2 = request.form.get["c2"]
+    except:
+        pass
+    try:
+        c3 = request.form.get["c3"]
+    except:
+        pass
+    try:
+        c4 = request.form.get["c4"]
+    except:
+        pass
+    try:
+        c5 = request.form.get["c5"]
+    except:
+        pass
     return "yay"
 
 
