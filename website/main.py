16ce985e2e333bfaa325d80bda403d30816b1642
ejmejm <ninjaedan@aol.com>
2017-12-03 03:16:53 -0500

    backend prediction

--- website/main.py
+++ website/main.py
@@ -1,6 +1,7 @@
 import zipcode
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
+import regression_file
 
 app = Flask(__name__)  # create the application instance :)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
